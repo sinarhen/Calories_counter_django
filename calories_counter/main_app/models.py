5fd7c8c6994f6ea2b78c6cfe4231e0609e2606a8
sinarhen <nicklchannel8@gmail.com>
2022-10-04 22:49:50 +0300

    Writed models, (More in the structure.png file)

--- calories_counter/main_app/models.py
+++ calories_counter/main_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import os
+
 
 # Create your models here.
+def create_path(instance, filename):
+    return os.path.join(
+        instance.name,
+        filename
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=255, blank=False)
+    price = models.DecimalField(verbose_name="Average Price", blank=False, decimal_places=1)
+    calories = models.IntegerField(verbose_name="Calories", blank=False)
+    protein = models.DecimalField(verbose_name="Protein", decimal_places=1, default=0.0)
+    carbohydrate = models.DecimalField(verbose_name="Carbohydrate", decimal_places=1, default=0.0)
+    fats = models.DecimalField(verbose_name="Fats", decimal_places=1, default=0.0)
+    photo = models.ImageField(verbose_name='Photo', db_index=True, upload_to=create_path)
+    type_of_food = models.ForeignKey('FoodType', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodType(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=255)
+    description = models.TextField(verbose_name='Description')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'FoodType'
+        verbose_name_plural = 'FoodTypes'
+        ordering = (id,)
