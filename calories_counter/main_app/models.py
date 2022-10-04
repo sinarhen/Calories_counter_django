aea0300567d7a66e2a07a4f1af77922ba5369d47
sinarhen <nicklchannel8@gmail.com>
2022-10-04 22:55:03 +0300

    Writed models, (More in the structure.png file)

--- calories_counter/main_app/models.py
+++ calories_counter/main_app/models.py
@@ -12,13 +12,13 @@ def create_path(instance, filename):
 
 class Product(models.Model):
     name = models.CharField(verbose_name="Name", max_length=255, blank=False)
-    price = models.DecimalField(verbose_name="Average Price", blank=False, decimal_places=1)
+    price = models.DecimalField(verbose_name="Average Price", blank=False, max_digits=10, decimal_places=1)
     calories = models.IntegerField(verbose_name="Calories", blank=False)
     protein = models.DecimalField(verbose_name="Protein", max_digits=4, decimal_places=1, default=0.0)
     carbohydrate = models.DecimalField(verbose_name="Carbohydrate", max_digits=4, decimal_places=1, default=0.0)
     fats = models.DecimalField(verbose_name="Fats", decimal_places=1, max_digits=4, default=0.0)
-    photo = models.ImageField(verbose_name='Photo', db_index=True, max_digits=4, upload_to=create_path)
-    type_of_food = models.ForeignKey('FoodType', max_digits=4, on_delete=models.PROTECT)
+    photo = models.ImageField(verbose_name='Photo', db_index=True, upload_to=create_path)
+    type_of_food = models.ForeignKey('FoodType', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
