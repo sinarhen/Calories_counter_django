9210139c421f9d6df52568a2db25320e28b6c1a6
sinarhen <nicklchannel8@gmail.com>
2022-12-08 16:54:47 +0200

    urls for static

--- calories_counter/calories_counter/urls.py
+++ calories_counter/calories_counter/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main_app.urls")),
     path('cart/', include("cart.urls"))
 ]
+if not settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>/', serve,
+                            {'insecure': True}))
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
